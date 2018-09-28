91ccbe424f1b1bed53fbcae0fc143a15bb9a7d22
Steven Whitaker <swhitaker@ebsco.com>
2018-09-28 14:20:16 -0500

    working status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Analytics } from './analytics/Analytics';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { setVal } from './reducers';
+import Main from './components/Main';
 
+const store = createStore(setVal, { testVal: 9999 });
+const initAnalytics = {
+  providerKey: null,
+  withLogging: true,
+  withOptimizely: false,
+  active: true
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Analytics value={initAnalytics}>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Analytics Wrapper Strawman</h1>
+            </header>
+            <p className="App-intro">
+              <Main/>
+            </p>
+          </div>
+        </Analytics>
+      </Provider>
     );
   }
 }
